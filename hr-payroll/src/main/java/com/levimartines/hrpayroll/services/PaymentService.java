32aa9e9c4f7c52283c4484338ae50655b2b55d70
Levi Martines <levi.mferreira@gmail.com>
2021-07-16 22:01:23 -0300

    feat: comunicação entre microserviços utilizando restTemplate

--- hr-payroll/src/main/java/com/levimartines/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/levimartines/hrpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.levimartines.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.levimartines.hrpayroll.models.Payment;
+import com.levimartines.hrpayroll.models.Worker;
 
 @Service
 public class PaymentService {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${hr-worker.host}")
+	private String workersHost;
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", String.valueOf(workerId));
+
+		Worker worker = restTemplate.getForObject(workersHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
