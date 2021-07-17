69872f1496c62f79880b04335446021c81b7198c
Levi Martines <levi.mferreira@gmail.com>
2021-07-16 22:45:18 -0300

    feat: balanceamento de carga com Netflix Ribbon

--- hr-worker/src/main/java/com/levimartines/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/levimartines/hrworker/controllers/WorkerController.java
@@ -1,6 +1,7 @@
 package com.levimartines.hrworker.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,15 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 import com.levimartines.hrworker.models.Worker;
 import com.levimartines.hrworker.repositories.WorkerRepository;
 
 @RestController
 @RequestMapping("/workers")
+@Slf4j
 public class WorkerController {
 
 	@Autowired
 	private WorkerRepository repository;
+
+	@Autowired
+	private Environment environment;
 
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
@@ -27,6 +34,7 @@ public class WorkerController {
 	@GetMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public Worker findById(@PathVariable Long id) {
+		log.info("PORT: {}", environment.getProperty("local.server.port"));
 		return repository.findById(id).orElse(null);
 	}
 }
