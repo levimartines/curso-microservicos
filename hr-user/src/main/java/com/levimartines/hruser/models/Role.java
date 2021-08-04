2f6330e3e9c49eac73ba70770ab0a36830288026
Levi Martines <levi.mferreira@gmail.com>
2021-08-03 21:26:13 -0300

    modelos User, Role e associacoes

---
+++ hr-user/src/main/java/com/levimartines/hruser/models/Role.java
@@ -0,0 +1,43 @@
+package com.levimartines.hruser.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roleName;
+
+	public Role() {
+	}
+
+	public Role(Long id, String roleName) {
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
