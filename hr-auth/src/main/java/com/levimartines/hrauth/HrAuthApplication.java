2d7353a086f512547063435fddd666b53ec81c14
Levi Martines <levi.mferreira@gmail.com>
2021-08-04 09:24:48 -0300

    criando e configurando projeto hr-auth

---
+++ hr-auth/src/main/java/com/levimartines/hrauth/HrAuthApplication.java
@@ -0,0 +1,15 @@
+package com.levimartines.hrauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class HrAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAuthApplication.class, args);
+	}
+
+}
