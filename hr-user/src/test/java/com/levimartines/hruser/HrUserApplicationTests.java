da8ee61bfc4de7f27af614cbfbfbb4dbc68e4619
Levi Martines <levi.mferreira@gmail.com>
2021-08-03 21:13:55 -0300

    criado microserviço hr-user

---
+++ hr-user/src/test/java/com/levimartines/hruser/HrUserApplicationTests.java
@@ -0,0 +1,14 @@
+package com.levimartines.hruser;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
